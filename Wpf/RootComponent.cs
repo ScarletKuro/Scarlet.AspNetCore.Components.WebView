0058f759e2e4ebd7519b30ce33aeade4e39a05b8
ScarletKuro <artem.melnikov@live.com>
2026-04-14 00:07:45 +0300

    WPF RootComponent add binding feature

--- Wpf/RootComponent.cs
+++ Wpf/RootComponent.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Windows;
 using Microsoft.AspNetCore.Components.WebView.WebView2;
 
 namespace Microsoft.AspNetCore.Components.WebView.Wpf
@@ -11,23 +12,71 @@ namespace Microsoft.AspNetCore.Components.WebView.Wpf
 	/// <summary>
 	/// Describes a root component that can be added to a <see cref="BlazorWebView"/>.
 	/// </summary>
-	public class RootComponent
+	/// <remarks>
+	/// Inherits from <see cref="Freezable"/> so that XAML bindings on <see cref="Selector"/>,
+	/// <see cref="ComponentType"/>, and <see cref="Parameters"/> pick up the hosting
+	/// <see cref="BlazorWebView"/>'s <c>DataContext</c> even though items inside
+	/// <see cref="RootComponentsCollection"/> are not part of the logical or visual tree.
+	/// </remarks>
+	public class RootComponent : Freezable
 	{
+		/// <summary>
+		/// Identifies the <see cref="Selector"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty SelectorProperty = DependencyProperty.Register(
+			nameof(Selector),
+			typeof(string),
+			typeof(RootComponent),
+			new PropertyMetadata(null));
+
+		/// <summary>
+		/// Identifies the <see cref="ComponentType"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty ComponentTypeProperty = DependencyProperty.Register(
+			nameof(ComponentType),
+			typeof(Type),
+			typeof(RootComponent),
+			new PropertyMetadata(null));
+
+		/// <summary>
+		/// Identifies the <see cref="Parameters"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty ParametersProperty = DependencyProperty.Register(
+			nameof(Parameters),
+			typeof(IDictionary<string, object?>),
+			typeof(RootComponent),
+			new PropertyMetadata(null));
+
 		/// <summary>
 		/// Gets or sets the CSS selector string that specifies where in the document the component should be placed.
 		/// This must be unique among the root components within the <see cref="BlazorWebView"/>.
 		/// </summary>
-		public string Selector { get; set; } = default!;
+		public string Selector
+		{
+			get => (string)GetValue(SelectorProperty);
+			set => SetValue(SelectorProperty, value);
+		}
 
 		/// <summary>
 		/// Gets or sets the type of the root component. This type must implement <see cref="IComponent"/>.
 		/// </summary>
-		public Type ComponentType { get; set; } = default!;
+		public Type ComponentType
+		{
+			get => (Type)GetValue(ComponentTypeProperty);
+			set => SetValue(ComponentTypeProperty, value);
+		}
 
 		/// <summary>
 		/// Gets or sets an optional dictionary of parameters to pass to the root component.
 		/// </summary>
-		public IDictionary<string, object?>? Parameters { get; set; }
+		public IDictionary<string, object?>? Parameters
+		{
+			get => (IDictionary<string, object?>?)GetValue(ParametersProperty);
+			set => SetValue(ParametersProperty, value);
+		}
+
+		/// <inheritdoc />
+		protected override Freezable CreateInstanceCore() => new RootComponent();
 
 		internal Task AddToWebViewManagerAsync(WebViewManager webViewManager)
 		{
